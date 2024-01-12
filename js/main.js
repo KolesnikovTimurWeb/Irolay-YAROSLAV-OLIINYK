9a6cc25d991e366b24a2ab650ab2631e0e2b43f6
KolesnikovTimurWeb <kolesnikovtimyrweb@gmail.com>
2024-01-12 22:26:37 +0200

    new

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector('.header .burger')
+let menu = document.querySelector('.header .menu')
+
+burger.addEventListener('click', function () {
+   menu.classList.toggle('active')
+   console.log('asdasd')
+})
